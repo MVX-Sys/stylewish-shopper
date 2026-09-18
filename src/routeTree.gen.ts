7bc4a397bfa926fb03c8c1607906edd62c50ef3b
WebMasterH256 <MatZeraH64@hotmail.com>
2026-09-18 01:20:34 -0300

    .

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -307,6 +307,7 @@ export interface FileRouteTypes {
     | '/_authenticated/admin/produtos/novo'
   fileRoutesById: FileRoutesById
 }
+
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   AuthenticatedRouteRoute: typeof AuthenticatedRouteRouteWithChildren
